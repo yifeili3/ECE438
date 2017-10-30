7265bcb6895015b158f5b6d9ebcaebe4eedc7a81
Rui Yang <ry2@illinois.edu>
2017-10-30 13:12:17 -0500

    Most complex version

--- MP2/sender.c
+++ MP2/sender.c
@@ -15,7 +15,7 @@ int send_base = 0, next_send = 0, file_pt = 0;
 int send_base_acked = 0, global_seq_num = 0;
 
 // Timing related
-int64_t timeout, estimatedRTT = 1000, deviation = 1;
+int64_t timeout = 500000, estimatedRTT = 100000, deviation = 1;
 uint64_t start_time;
 
 // Tmp parameters or flags
@@ -30,6 +30,22 @@ int64_t sent_time[SWND];
 char buf[sizeof(packet)];
 int soc_state = CLOSED;
 
+void printSWNDseq() {
+    cout << "Current seq # in SWND: ";
+    for (int j = 0; j < SWND; ++j ) {
+        cout << window_buffer[j].seq_num << "  ";
+    }
+    cout << endl;
+}
+
+void printSentTime() {
+    cout << "Send time vector: " << endl;
+    for (int i = 0; i < SWND; ++i) {
+        cout << sent_time[i] << " ";
+    }
+    cout << endl << "with timeout: " << timeout << endl;
+}
+
 void openFile(char* filename, unsigned long long int bytesToTransfer) {
     // Open the file
     fp = fopen(filename, "rb");
@@ -39,26 +55,27 @@ void openFile(char* filename, unsigned long long int bytesToTransfer) {
     }
 
     /* Determine how many bytes to transfer */
-    fseek (fp, 0, SEEK_END);
-    unsigned long long int file_size = ftell(fp);
-    rewind(fp);
-    bytesToRead = min(file_size, bytesToTransfer);
+//    fseek (fp, 0, SEEK_END);
+//    unsigned long long int file_size = ftell(fp);
+//    rewind(fp);
+//    bytesToRead = min(file_size, bytesToTransfer);
+    bytesToRead = bytesToTransfer;
 
     num_pkt_total = (unsigned long long int) ceil(bytesToRead * 1.0 / MSS);
     cout << num_pkt_total << endl;
 }
 
-uint64_t time_now() {
+uint64_t timeNow() {
     struct timeval current;
     gettimeofday(&current, 0);
     return (uint64_t)(current.tv_sec * 1000000 + current.tv_usec);
 }
 
-int64_t proc_time_now() {
-    return (int64_t) (time_now() - start_time);
+int64_t procTimeNow() {
+    return (int64_t) (timeNow() - start_time);
 }
 
-/*
+
 int setTimeout(int sockfd, int usec)
 {
     if (usec < 0)
@@ -68,16 +85,16 @@ int setTimeout(int sockfd, int usec)
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
         perror("sender: setsockopt");
     }
+    cout << "Setting timeout to " << timeout << endl;
     return 0;
 }
- */
 
 void updateTimeout(int64_t sentTime) {
-    int64_t sampleRTT = time_now() - sentTime;
+    int64_t sampleRTT = procTimeNow() - sentTime;
     estimatedRTT = (int64_t)(0.875 * estimatedRTT + 0.125 * sampleRTT); // alpha = 0.875
     deviation += (int64_t)(0.25 * ( abs(sampleRTT - estimatedRTT) - deviation)); //delta = 0.25
     timeout = (estimatedRTT + 4 * deviation); // mu = 1, phi = 4
-//    timeout = timeout/5;
+//    timeout = 100000;
 }
 
 int handshake(int sockfd) {
@@ -139,6 +156,8 @@ int buildSenderSocket(char* hostname, char* hostUDPport) {
 		return 2;
 	}
 
+//    return sockfd;
+
 	if (handshake(sockfd) != -1) {
     	return sockfd;
     } else {
@@ -149,11 +168,17 @@ int buildSenderSocket(char* hostname, char* hostUDPport) {
 }
 
 int fillingWindow(int num_pkt) {
-    int counter = 0;
+    if (num_pkt == 0) { return 0; }    
+    int counter = 0, byte_trans_once = 0;
     char file_buffer[MSS+1];
     packet pkt;
     for (counter = 0; bytesToRead && counter < num_pkt; ++ counter) {
-        int byte_trans_once = min(MSS, (int) bytesToRead);
+        if (bytesToRead < (unsigned long long int) MSS) {
+            byte_trans_once = (int) bytesToRead;
+        } else {
+            byte_trans_once = MSS;
+        }
+//        byte_trans_once = min(MSS, (int) bytesToRead);
         int read_size = fread (file_buffer, sizeof(char), byte_trans_once, fp);
         if (read_size > 0) {
             pkt.data_size = read_size;
@@ -167,12 +192,13 @@ int fillingWindow(int num_pkt) {
         bytesToRead -= read_size;
     }
 
-    cout << "Filling the window" << endl;
+    cout << "Filling the window by size " << num_pkt << endl;
     return counter;
 }
 
 int sendMultiPackets (int sockfd, int start_pt, int last_pt) {
     // Inclusive for start and last
+    if (start_pt >= last_pt) { return 0; }
     for (int i = start_pt; i < last_pt; ++i) {
         int i_pos_in_swnd = i % SWND;
         memcpy(buf, &window_buffer[i_pos_in_swnd], sizeof(packet));
@@ -182,10 +208,9 @@ int sendMultiPackets (int sockfd, int start_pt, int last_pt) {
             exit(2);
         }
         cout << "-----------Sending packet " << window_buffer[i_pos_in_swnd].seq_num << "---------------" << endl;
-        sent_time[i_pos_in_swnd] = proc_time_now();
+        sent_time[i_pos_in_swnd] = procTimeNow();
     }
     num_pkt_sent += (last_pt - start_pt) % SWND;
-    if (!file_read_finished) { fillingWindow( (last_pt- start_pt) % SWND ); }
     return 0;
 }
 
@@ -196,12 +221,12 @@ int sendSinglePacket (int sockfd, int pt) {
         perror("Error: data sending (single)");
         exit(2);
     }
-    sent_time[pt] = proc_time_now();
+    cout << "-----------Sending single packet " << window_buffer[pt].seq_num << "---------------" << endl;
+    sent_time[pt] = procTimeNow();
     return 0;
 }
 
 int sendAllowedPackets (int sockfd) {
-
     if (num_pkt_sent == num_pkt_total) {
         cout << "No packet to send" << endl;
         return 0;
@@ -211,17 +236,28 @@ int sendAllowedPackets (int sockfd) {
     if (next_send < send_base) {
         next_send += SWND;
     }
-    if (num_pkt_total - num_pkt_sent < last_pk - next_send ) {
+    if ((long long int) (num_pkt_total - num_pkt_sent) < last_pk - next_send ) {
+        cout << "Near the end with " << num_pkt_total - num_pkt_sent
+             << " remaining. Initially:  " << last_pk - next_send << endl;
         last_pk = next_send + num_pkt_total - num_pkt_sent;
     }
 
-    sendMultiPackets(sockfd, next_send, last_pk);
-    next_send = (last_pk) % SWND;
+    cout << "base: " << send_base << " cwnd: " << cwnd
+         <<" base+cwnd: " << last_pk
+         << " next_send: " << next_send << endl;
+    if (last_pk <= next_send) {
+        cout << "CWND is too small to send." << endl;
+    } else {
+        sendMultiPackets(sockfd, next_send, last_pk);
+    }
+
+    next_send = max(next_send, last_pk) % SWND;
     cout << "send_base: " << send_base << " next_send: " << next_send << endl;
     return 0;
 }
 
 int handleACK (packet pkt, int sockfd) {
+    cout << "~~~~ Receiving packet " << pkt.ack_num << " ~~~~" << endl;
     int ack_pos_in_swnd = pkt.ack_num % SWND;
     if ((send_base < next_send && (ack_pos_in_swnd > next_send || ack_pos_in_swnd < send_base)) ||
         (send_base > ack_pos_in_swnd && ack_pos_in_swnd > next_send )) {
@@ -232,8 +268,9 @@ int handleACK (packet pkt, int sockfd) {
     if ((ack_pos_in_swnd > send_base) ||
             (next_send < send_base && ack_pos_in_swnd <= next_send)) {
         num_pkt_received += (pkt.ack_num - send_base) % SWND;
-        send_base = ack_pos_in_swnd;
         cout << "Received a new ACK with " << pkt.ack_num << endl;
+        fillingWindow ((pkt.ack_num - send_base) % SWND);
+        send_base = ack_pos_in_swnd;
         // new ACK
         if (soc_state == FAST_RECOVERY) {
             cwnd = ssthread * 1.0;
@@ -261,9 +298,19 @@ int handleACK (packet pkt, int sockfd) {
                 perror("Wrong socket status");
                 return -1;
         }
+//        updateTimeout(sent_time[ack_pos_in_swnd]);
+//        setTimeout(sockfd, timeout * 5);
     } else if (ack_pos_in_swnd == send_base) { // dup ACK
+        printSWNDseq();
         if (soc_state == SLOW_START || soc_state == CONGESTION_AVOID) {
             ++dupACK;
+            // dupACK to 3
+            if (dupACK >= 3) {
+                ssthread = (int) cwnd / 2;
+                cwnd = ssthread + 3.0;
+                sendSinglePacket(sockfd, send_base);
+                soc_state = FAST_RECOVERY;
+            }
         } else { cwnd = (cwnd + 1 < SWND) ? cwnd + 1 : SWND - 1; }
     } else {
         perror("Invalid ACK packet");
@@ -274,32 +321,26 @@ int handleACK (packet pkt, int sockfd) {
         soc_state = CONGESTION_AVOID;
     }
 
-    // dupACK to 3
-    if (dupACK >= 3) {
-        ssthread = (int) cwnd / 2;
-        cwnd = ssthread + 3.0;
-        sendSinglePacket(sockfd, send_base);
-        soc_state = FAST_RECOVERY;
-    }
-    updateTimeout(sent_time[ack_pos_in_swnd]);
+//    updateTimeout(sent_time[ack_pos_in_swnd]);
     cout << "State: " << soc_state << "  cwnd: " << cwnd << " ssthread: " << ssthread
-         << " dupACK: " << dupACK << endl;
+         << " dupACK: " << dupACK << " send_base: " << send_base
+         << " timeout: " << timeout << endl;
     sendAllowedPackets(sockfd);
 
     return 0;
 }
 
 void endConnection(int sockfd){
-    packet pkt;
-    pkt.msg_type = FIN;
-    pkt.data_size=0;
-    memcpy(buf, &pkt, sizeof(packet));
-    if((numbytes = sendto(sockfd, buf, sizeof(packet), 0, p->ai_addr, p->ai_addrlen))== -1){
-        perror("can not send FIN to sender");
-        exit(2);
-    }
     // Wait for two ACK
+    packet pkt;
     while (1) {
+        pkt.msg_type = FIN;
+        pkt.data_size=0;
+        memcpy(buf, &pkt, sizeof(packet));
+        if((numbytes = sendto(sockfd, buf, sizeof(packet), 0, p->ai_addr, p->ai_addrlen))== -1){
+            perror("can not send FIN to sender");
+            exit(2);
+        }
         packet ack;
         if ((numbytes = recvfrom(sockfd, buf, sizeof(packet), 0, (struct sockaddr *) &their_addr, &addr_len)) == -1) {
             perror("can not receive from sender");
@@ -351,17 +392,42 @@ void reliablyTransfer(char* hostname, char* hostUDPport, char* filename, unsigne
     }
 
     // Time started:
-    start_time = time_now();
+    start_time = timeNow();
+    setTimeout(sockfd, timeout);
 
     sendAllowedPackets(sockfd);
 //    sendSinglePacket(sockfd, 0);
 //    setTimeout(sockfd, (int) estimatedRTT);
     while (num_pkt_sent < num_pkt_total ||
            num_pkt_received < num_pkt_sent) {
+        // Check for timeout
+        uint64_t current_time = procTimeNow();
+        for (int i = 0; i < (int) cwnd; ++i) {
+            int i_pos_in_swnd = (send_base + i) % SWND;
+            if (current_time - sent_time[i_pos_in_swnd] < timeout) { continue; }
+            memcpy(buf, &window_buffer[i_pos_in_swnd], sizeof(packet));
+            if((numbytes = sendto(sockfd, buf, sizeof(packet), 0, p->ai_addr, p->ai_addrlen))== -1){
+                perror("Error: timeout sending");
+                printf("Fail to send %d pkt in SWND", i_pos_in_swnd);
+                exit(2);
+            }
+            cout << "----- Timeout resending packet " << window_buffer[i_pos_in_swnd].seq_num << "----------"
+                 << endl << "t_now: " << current_time << "t have:" << sent_time[i_pos_in_swnd] << " diff: " << current_time - sent_time[i_pos_in_swnd] << endl;
+            printSentTime();
+            sent_time[i_pos_in_swnd] = procTimeNow();
+            // timeout state
+            ssthread = cwnd/2; cwnd = 1.0; dupACK = 0;
+            timeout = min(timeout * 2, (int64_t)2000000);
+            setTimeout(sockfd, timeout);
+            break; // Only resend the first one.
+        }
+
         // Wait for ack
         if((numbytes = recvfrom(sockfd, buf, sizeof(packet), 0, NULL, NULL)) == -1) {
-            perror("can not receive data");
-            exit(2);
+            if (errno != EAGAIN || errno != EWOULDBLOCK) {
+                perror("can not receive main ack");
+                exit(2);
+            }
         }
         packet pkt;
         memcpy(&pkt, buf, sizeof(packet));
@@ -375,6 +441,5 @@ void reliablyTransfer(char* hostname, char* hostUDPport, char* filename, unsigne
 
     endConnection(sockfd);
 
-    //TODO: timeout
     //FIXME: Sending too many packets
 }
